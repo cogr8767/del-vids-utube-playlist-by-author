9694385e28cd00feb59849b53f617d4884036678
Collin Graham <89661748+cogr8767@users.noreply.github.com>
2021-12-31 16:03:16 -0700

    cleaned up source.py

--- source.py
+++ source.py
@@ -1,14 +1,15 @@
 from Google import Create_Service
 
+## Edit This Information
+playlistId_Source = 'PLa2--cmNqUF6bm5F5Bk2740Fo8NTlCBTI'
+delChannelName = "Chris Hallbeck"
+
 CLIENT_SECRET_FILE  =  'client_secret_7.json'
 API_NAME  =  'youtube'
 API_VERSION  =  'v3'
 SCOPES  = ['https://www.googleapis.com/auth/youtube']
 
 service =  Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-
-
-playlistId_Source = 'watch_later'
 
 response = service.playlistItems().list(
     part='snippet',
@@ -32,6 +33,6 @@ while nextPageToken:
 
 
 for video in playlistItems: 
-    if video['snippet']['videoOwnerChannelTitle'] == "Mark165":
+    if video['snippet']['videoOwnerChannelTitle'] == delChannelName:
         print("Deleting Video by: " + video['snippet']['videoOwnerChannelTitle'])
         service.playlistItems().delete(id=video['id']).execute()
